db71f66b1ff3501f0619013bbdee54eb69009537
jorda <jordanlim1108@gmail.com>
2023-08-20 14:34:55 -0700

    Add basic prompting and computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+console.log("Hello");
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice == 1) return "Rock";
+  else if (choice == 2) return "Paper";
+  else return "Scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+  let input = toLowerCase(playerSelection);
+
+  switch (input){
+    case "rock":
+      
+
+
+
+    default:
+      console.log("Invalid input")
+  }
+}
+
+const playerSelection = prompt("What do you play?");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
